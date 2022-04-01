efe2c294a597f1b88208a8c83b68e076d428b8e0
hajimev7 <57899416+hajimev7@users.noreply.github.com>
2022-04-02 01:42:16 +0900

    create app

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyANEe7BfHEoECUBwLy48eDTaStmd2j1OCk",
+  authDomain: "idobatakaigi-with-ham-a67af.firebaseapp.com",
+  projectId: "idobatakaigi-with-ham-a67af",
+  storageBucket: "idobatakaigi-with-ham-a67af.appspot.com",
+  messagingSenderId: "59878027798",
+  appId: "1:59878027798:web:173a2d0514581b2572fb37"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+const messageRef=database.ref('messages')
+
+export const pushMessage = ({name,text}) =>{
+    messageRef.push({name,text})
+}
