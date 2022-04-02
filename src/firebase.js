6ab3ef3c7c8ef09c492fe027d2b50d10fd12bfcd
hajimev7 <57899416+hajimev7@users.noreply.github.com>
2022-04-02 11:14:47 +0900

    remove hard coded env vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,21 @@
 import firebase from "firebase";
 
+const  {
+REACT_APP_FIREBASE_API_KEY,
+REACT_APP_FIREBASE_AUTH_DOMAIN,
+REACT_APP_FIREBASE_PROJECT_ID,
+REACT_APP_FIREBASE_STORAGE_BUCKET,
+REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+REACT_APP_FIREBASE_APP_ID,
+}=process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyANEe7BfHEoECUBwLy48eDTaStmd2j1OCk",
-  authDomain: "idobatakaigi-with-ham-a67af.firebaseapp.com",
-  projectId: "idobatakaigi-with-ham-a67af",
-  storageBucket: "idobatakaigi-with-ham-a67af.appspot.com",
-  messagingSenderId: "59878027798",
-  appId: "1:59878027798:web:173a2d0514581b2572fb37"
+  apiKey:REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: REACT_APP_FIREBASE_APP_ID,
 };
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
